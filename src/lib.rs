d93adea10ae960d9725c6d5ce25915dece569910
keighbee <kb@huggingface.co>
2025-07-30 15:53:16 -0700

    lint

--- src/lib.rs
+++ src/lib.rs
@@ -210,11 +210,15 @@ impl TensorShape {
         }
     }
 
-    fn broadcast_shape(&self, other: &TensorShape, corresponding_dimensions: &[(usize, usize)]) -> TensorShape {
+    fn broadcast_shape(
+        &self,
+        other: &TensorShape,
+        corresponding_dimensions: &[(usize, usize)],
+    ) -> TensorShape {
         // Create mapping for corresponding dimensions
         let mut dim_correspondence = std::collections::HashMap::new();
         let mut other_dim_used = vec![false; other.shape.len()];
-        
+
         for &(self_dim, other_dim) in corresponding_dimensions {
             if self_dim >= self.shape.len() || other_dim >= other.shape.len() {
                 panic!("Dimension index out of bounds");
@@ -222,15 +226,15 @@ impl TensorShape {
             dim_correspondence.insert(self_dim, other_dim);
             other_dim_used[other_dim] = true;
         }
-        
+
         // Build output shape: LHS dimensions (with broadcasting) + remaining RHS dimensions
         let mut output_shape = Vec::new();
-        
+
         // Process LHS dimensions in order
         for (self_dim, &self_size) in self.shape.iter().enumerate() {
             if let Some(&other_dim) = dim_correspondence.get(&self_dim) {
                 let other_size = other.shape[other_dim];
-                
+
                 if self_size == other_size {
                     output_shape.push(self_size);
                 } else if self_size == 1 {
@@ -238,20 +242,23 @@ impl TensorShape {
                 } else if other_size == 1 {
                     output_shape.push(self_size);
                 } else {
-                    panic!("Cannot broadcast dimensions: {} and {}", self_size, other_size);
+                    panic!(
+                        "Cannot broadcast dimensions: {} and {}",
+                        self_size, other_size
+                    );
                 }
             } else {
                 output_shape.push(self_size);
             }
         }
-        
+
         // Add remaining RHS dimensions that weren't used in correspondence
         for (other_dim, &other_size) in other.shape.iter().enumerate() {
             if !other_dim_used[other_dim] {
                 output_shape.push(other_size);
             }
         }
-        
+
         TensorShape::new(output_shape)
     }
 }
@@ -287,7 +294,7 @@ impl<T: Clone> TensorStorage<T> {
         // Calculate result shape by removing the reduction dimension
         let mut result_shape_vec = shape.shape.clone();
         result_shape_vec.remove(dim);
-        
+
         if result_shape_vec.is_empty() {
             // Reducing to scalar
             let v = self
@@ -296,24 +303,24 @@ impl<T: Clone> TensorStorage<T> {
                 .cloned()
                 .reduce(|a, b| f(&a, &b))
                 .expect("Cannot reduce an empty tensor storage");
-            return TensorStorage { data: vec![v] }
+            return TensorStorage { data: vec![v] };
         }
-        
+
         let result_shape = TensorShape::new(result_shape_vec);
         let mut result_storage = TensorStorage::zeros(result_shape.size());
 
         // Iterate through all positions in the result tensor
         for result_flat_idx in 0..result_shape.size() {
             let result_multi_idx = result_shape.unravel_index(result_flat_idx);
-            
+
             // For each result position, reduce along the specified dimension
             let mut accumulated_value: Option<T> = None;
-            
+
             for dim_idx in 0..shape.shape[dim] {
                 // Reconstruct the full multi-index by inserting the dimension index
                 let mut full_multi_idx = Vec::with_capacity(shape.shape.len());
                 let mut result_idx_pos = 0;
-                
+
                 for d in 0..shape.shape.len() {
                     if d == dim {
                         full_multi_idx.push(dim_idx);
@@ -322,16 +329,16 @@ impl<T: Clone> TensorStorage<T> {
                         result_idx_pos += 1;
                     }
                 }
-                
+
                 let source_flat_idx = shape.linear_offset + shape.ravel_index(&full_multi_idx);
                 let value = &self[source_flat_idx];
-                
+
                 accumulated_value = match accumulated_value {
                     None => Some(value.clone()),
                     Some(acc) => Some(f(&acc, value)),
                 };
             }
-            
+
             result_storage[result_flat_idx] = accumulated_value.unwrap();
         }
 
@@ -339,12 +346,12 @@ impl<T: Clone> TensorStorage<T> {
     }
 
     fn broadcast_op<F, U, T2>(
-        &self, 
+        &self,
         self_shape: &TensorShape,
-        other: &TensorStorage<T2>, 
+        other: &TensorStorage<T2>,
         other_shape: &TensorShape,
-        corresponding_dimensions: &[(usize, usize)], 
-        f: F
+        corresponding_dimensions: &[(usize, usize)],
+        f: F,
     ) -> TensorStorage<U>
     where
         F: Fn(&T, &T2) -> U,
@@ -352,35 +359,35 @@ impl<T: Clone> TensorStorage<T> {
     {
         let result_shape = self_shape.broadcast_shape(other_shape, corresponding_dimensions);
         let mut result = TensorStorage::<U>::zeros(result_shape.size());
-        
+
         // Create mapping for corresponding dimensions
         let mut dim_correspondence = std::collections::HashMap::new();
         let mut other_dim_used = vec![false; other_shape.shape.len()];
-        
+
         for &(self_dim, other_dim) in corresponding_dimensions {
             dim_correspondence.insert(self_dim, other_dim);
             other_dim_used[other_dim] = true;
         }
-        
+
         // Perform the element-wise operation
         for flat_idx in 0..result_shape.size() {
             let output_multi_idx = result_shape.unravel_index(flat_idx);
-            
+
             // Map output indices to input tensor indices
             let mut self_idx = vec![0; self_shape.shape.len()];
             let mut other_idx = vec![0; other_shape.shape.len()];
-            
+
             // Map LHS dimensions
             for (self_dim, &self_size) in self_shape.shape.iter().enumerate() {
                 let output_val = output_multi_idx[self_dim];
-                
+
                 if let Some(&other_dim) = dim_correspondence.get(&self_dim) {
                     if self_size == 1 {
                         self_idx[self_dim] = 0;
                     } else {
                         self_idx[self_dim] = output_val;
                     }
-                    
+
                     if other_shape.shape[other_dim] == 1 {
                         other_idx[other_dim] = 0;
                     } else {
@@ -390,7 +397,7 @@ impl<T: Clone> TensorStorage<T> {
                     self_idx[self_dim] = output_val;
                 }
             }
-            
+
             // Map remaining RHS dimensions
             let mut rhs_output_offset = self_shape.shape.len();
             for (other_dim, _) in other_shape.shape.iter().enumerate() {
@@ -399,18 +406,18 @@ impl<T: Clone> TensorStorage<T> {
                     rhs_output_offset += 1;
                 }
             }
-            
+
             // Get values from input tensors using proper indexing
             let self_flat = self_shape.linear_offset + self_shape.ravel_index(&self_idx);
             let other_flat = other_shape.linear_offset + other_shape.ravel_index(&other_idx);
-            
+
             let self_val = &self[self_flat];
             let other_val = &other[other_flat];
-            
+
             // Apply operation and store result
             result[flat_idx] = f(self_val, other_val);
         }
-        
+
         result
     }
 }
@@ -512,7 +519,7 @@ impl<T: Zero + Clone> Tensor<T> {
         }
 
         let result_storage = self.storage.reduce(&self.shape, dim, f);
-        
+
         // Calculate result shape by removing the reduction dimension
         let mut result_shape_vec = self.shape.shape.clone();
         result_shape_vec.remove(dim);
@@ -524,13 +531,20 @@ impl<T: Zero + Clone> Tensor<T> {
         }
     }
 
-    fn broadcast_op<F, U, T2>(&self, other: &Tensor<T2>, corresponding_dimensions: &[(usize, usize)], f: F) -> Tensor<U>
+    fn broadcast_op<F, U, T2>(
+        &self,
+        other: &Tensor<T2>,
+        corresponding_dimensions: &[(usize, usize)],
+        f: F,
+    ) -> Tensor<U>
     where
         F: Fn(&T, &T2) -> U,
         U: Zero + Clone,
     {
-        let result_shape = self.shape.broadcast_shape(&other.shape, corresponding_dimensions);
-        
+        let result_shape = self
+            .shape
+            .broadcast_shape(&other.shape, corresponding_dimensions);
+
         let result_storage = self.storage.broadcast_op(
             &self.shape,
             &other.storage,
@@ -538,7 +552,7 @@ impl<T: Zero + Clone> Tensor<T> {
             corresponding_dimensions,
             f,
         );
-        
+
         Tensor {
             shape: result_shape,
             storage: result_storage,
